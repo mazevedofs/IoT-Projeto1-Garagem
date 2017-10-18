4efe17e6f24d44779a05449b836282e250629c89
RHAYSSA CHRISTINA MARCIANO DE SOUZA OLIVEIRA <35997102874@jpmorgan.sp>
2017-10-18 20:43:15 -0200

    ServoMotor

---
+++ Garagem-Motor/servo.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+#define SERVO 6 // Porta Digital 6 PWM
+
+Servo portao; // Variável Servo
+int pos; // Posição Servo
+
+void setup ()
+{
+  portao.attach(SERVO);
+  Serial.begin(9600);
+  portao.write(0); // Inicia motor posição zero
+
+
+}
+
+void loop()
+{
+
+  AbrePortao();
+
+
+}
+
+void AbrePortao() {
+  for (pos = 0; pos < 90; pos++)
+  {
+    portao.write(pos);
+    delay(15);
+  }
+
+  delay(1000);
+
+  void FechaPortao();
+
+  for (pos = 90; pos >= 0; pos--)
+  {
+    portao.write(pos);
+    delay(15);
+  }
+}
+
+
+
+
+
+
+
